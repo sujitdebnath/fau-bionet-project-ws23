458dd1c8f8191e91e9babc9ef713d14a34d4fc2b
FarzamTP <farzam.taghipour@gmail.com>
2024-02-12 16:08:40 +0100

    Dash app is now supporting two diseases

--- WebApplication/dash_app.py
+++ WebApplication/dash_app.py
@@ -1,17 +1,25 @@
 import os
+import shutil
 
 from dash import Dash, html, dcc, Input, Output, no_update, callback
 import dash_bootstrap_components as dbc
 import dash_ag_grid as dag
 import pandas as pd
-from dash.exceptions import PreventUpdate
-
-# all_cell_DEG_df = pd.read_csv("DEG_All.csv")
-# specific_cell_DEG_df = pd.read_csv("DEG_T cell.csv")
 
 app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], serve_locally=True)
 
-dataset_files = [file_name.replace('.csv', '') for file_name in os.listdir('./resulted_DEG_files')]
+if os.path.exists('DEG_results'):
+    shutil.rmtree('DEG_results')
+    shutil.copytree('./../Cell_Annotation_and_DGE/DEG_results', 'DEG_results')
+else:
+    shutil.copytree('./../Cell_Annotation_and_DGE/DEG_results', 'DEG_results')
+
+
+diseases_list = ['DiabetesII', 'MPN']
+
+datasets = [file_name.replace('.csv', '') for file_name in os.listdir(f'./DEG_results/{diseases_list[0]}')]
+
+datasets.sort()
 
 columnDefs = [
     {"field": "Gene", 'text-align': 'center'},
@@ -54,9 +62,14 @@ app.layout = dbc.Container([
                 [
                     html.H1("BioNet Project", style={'color': 'white', 'text-align': 'center'}),
                     html.Div([
-                        html.H3("Results for Differential Gene Analysis for MPN Disease",
+                        html.Br(),
+                        html.H3('Select the disease to load analysis', style={'color': 'white', 'margin': '30px'}),
+                        dcc.Dropdown(diseases_list, value=diseases_list[0], id='disease_name',
+                                     style={'margin': '30px', 'width': '50%'}),
+                        html.H3(id='disease_name_banner',
                                 style={'color': 'white', 'margin': '30px'}),
                     ], style={'border-style': 'solid', 'border-color': 'red'}),
+
                 ],
                 width=12,
             ),
@@ -67,7 +80,7 @@ app.layout = dbc.Container([
     dbc.Row([
         dbc.Col([
             html.H4("First Dataset:", style={'color': 'white'}),
-            dcc.Dropdown(dataset_files, value=dataset_files[0], id='first_table_dropdown'),
+            dcc.Dropdown(datasets, value=datasets[0], id='first_table_dropdown'),
             html.Br(),
             html.H6("Go to page:", style={'color': 'white'}),
             dcc.Input(id="first_table_page_input", type="number", style={'font-size': 'large'}, value=1),
@@ -80,7 +93,7 @@ app.layout = dbc.Container([
 
         dbc.Col([
             html.H4("Second Dataset:", style={'color': 'white'}),
-            dcc.Dropdown(dataset_files, value=dataset_files[1], id='second_table_dropdown'),
+            dcc.Dropdown(datasets, value=datasets[1], id='second_table_dropdown'),
             html.Br(),
             html.H6("Go to page:", style={'color': 'white'}),
             dcc.Input(id="second_table_goto_page_input", type="number", style={'font-size': 'large'}, value=1),
@@ -96,21 +109,35 @@ app.layout = dbc.Container([
 
 
 @callback(
-    Output('first_table_grid', 'rowData'),
-    Input('first_table_dropdown', 'value')
+    [Output('first_table_dropdown', 'options'),
+     Output('second_table_dropdown', 'options'),
+     Output('disease_name_banner', 'children')],
+    Input('disease_name', 'value')
 )
-def update_first_table(value):
-    first_dataset = pd.read_csv(f'./resulted_DEG_files/{str(value)}.csv')
-    return first_dataset.to_dict("records")
+def update_disease(disease_name):
+    datasets = [file_name.replace('.csv', '') for file_name in os.listdir(f'./DEG_results/{disease_name}')]
+    datasets.sort()
+    return datasets, datasets, f'Results for Differential Gene Analysis for {disease_name} Disease'
+
+
+@callback(
+    Output('first_table_grid', 'rowData'),
+    [Input('first_table_dropdown', 'value'),
+     Input('disease_name', 'value')]
+)
+def update_second_table(dataset_name, disease_name):
+    dataset = pd.read_csv(f'./DEG_results/{disease_name}/{dataset_name}/DEG_All.csv')
+    return dataset.to_dict("records")
 
 
 @callback(
     Output('second_table_grid', 'rowData'),
-    Input('second_table_dropdown', 'value')
+    [Input('second_table_dropdown', 'value'),
+     Input('disease_name', 'value')]
 )
-def update_first_table(value):
-    first_dataset = pd.read_csv(f'./resulted_DEG_files/{str(value)}.csv')
-    return first_dataset.to_dict("records")
+def update_second_table(dataset_name, disease_name):
+    dataset = pd.read_csv(f'./DEG_results/{disease_name}/{dataset_name}/DEG_All.csv')
+    return dataset.to_dict("records")
 
 
 @callback(
