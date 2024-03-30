3cd8b6576800063f9a0fccaf0e6b6e3b1ad3316d
Sujit Debnath <sujit.debnath.bd@gmail.com>
2024-03-30 14:33:39 +0100

    Update run script for adata preprocessor

--- pipelines/run.sh
+++ pipelines/run.sh
@@ -6,10 +6,27 @@ disease_id="mpn"
 dataset_id="dataset1"
 
 # Run the data handler script with required arguments
-echo "----------------------------------- Data Extraction Started -----------------------------------"
-python3 "${PROJECT_DIR}/pipelines/services/data_handler.py" \
+echo "----------------------------------- Data Handler Started -----------------------------------"
+python3 "${PROJECT_DIR}/pipelines/services/adata_handler.py" \
     --disease_id "${disease_id}" \
     --dataset_id "${dataset_id}"
-echo "----------------------------------- Data Extraction Ended -----------------------------------"
+echo "----------------------------------- Data Handler Ended -----------------------------------"
+
+echo ""
+
+# Run the data preprocessing script with required arguments
+echo "----------------------------------- Data Preprocessing Started -----------------------------------"
+python3 "${PROJECT_DIR}/pipelines/services/adata_preprocessor.py" \
+    --disease_id "${disease_id}" \
+    --dataset_id "${dataset_id}" \
+    --mito_perc 0.05 \
+    --n_umis 500 \
+    --detected_genes 250 \
+    --preprocess_mode 'shiftlog|pearson' \
+    --n_hvgs 2000 \
+    --n_pcs 50 \
+    --n_neighbors 15 \
+    --cluster_mode 'leiden'
+echo "----------------------------------- Data Preprocessing Ended -----------------------------------"
 
 echo ""
