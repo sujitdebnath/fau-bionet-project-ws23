6b15fafb409b444d221f3bdb004cfe90b17b4892
Sujit Debnath <sujit.debnath.bd@gmail.com>
2024-04-05 10:19:07 +0200

    Implemented cell annotation with MetaTiME

--- pipelines/services/cell_type_annotation.py
+++ pipelines/services/cell_type_annotation.py
@@ -6,19 +6,22 @@ import omicverse as ov
 import seaborn as sns
 import matplotlib.pyplot as plt
 from typing import Callable, Any, Tuple
+from metatime import config, mecmapper, mecs, annotator
 import adata_handler
 
 
 def parse_add_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     parser.add_argument('--disease_id', type=str, help='Unique identifier for disease, e.g., diabetesII, mpn')
     parser.add_argument('--dataset_id', type=str, help='Unique identifier of the dataset for that specific disease')
-    parser.add_argument('--cellmarker', action='store_true', help='Use cellmarker as the reference dataset for annotation')
-    parser.add_argument('--panglaodb', action='store_true', help='Use panglaodb as the reference dataset for annotation')
-    parser.add_argument('--foldchange', type=float, default=1.5, help='Fold change threshold')
-    parser.add_argument('--pvalue', type=float, default=0.01, help='P-value threshold')
-    parser.add_argument('--celltype', type=str, default='normal', help='Cell type for annotation')
-    parser.add_argument('--clustertype', type=str, default='leiden', help='Clustering name used in scanpy')
-    parser.add_argument('--rank_rep', action='store_true', help='Rank representation')
+    parser.add_argument('--anno_methods', nargs='+', type=str, default=['scsa', 'metatime'],
+                        help='Possible method list of automatic cell type annotation')
+    parser.add_argument('--cellmarker', action='store_true', help='Use cellmarker as the reference dataset for SCSA annotation')
+    parser.add_argument('--panglaodb', action='store_true', help='Use panglaodb as the reference dataset for SCSA annotation')
+    parser.add_argument('--foldchange', type=float, default=1.5, help='Fold change threshold for SCSA')
+    parser.add_argument('--pvalue', type=float, default=0.01, help='P-value threshold for SCSA')
+    parser.add_argument('--celltype', type=str, default='normal', help='Cell type for annotation for SCSA')
+    parser.add_argument('--clustertype', type=str, default='leiden', help='Clustering name used in scanpy for SCSA')
+    parser.add_argument('--rank_rep', action='store_true', help='Rank representation for SCSA')
 
     return parser
 
@@ -38,6 +41,39 @@ def apply_cell_type_anno_scsa(adata: sc.AnnData, cellmarker: bool, panglaodb: bo
         
         scsa.cell_anno(clustertype=clustertype, cluster='all', rank_rep=rank_rep)
         scsa.cell_auto_anno(adata, key='scsa_celltype_panglaodb')
+
+def apply_cell_type_anno_metatime(adata: sc.AnnData) -> None:
+    # MetaTiME using Omicverse
+    # metatime_obj = ov.single.MetaTiME(adata, mode='table')
+    # metatime_obj.overcluster(resolution=21, clustercol='overcluster')
+    # metatime_obj.predictTiME(save_obs_name='MetaTiME')
+    
+    # MetaTiME - manual
+    # ------------------------------------------------
+    adata         = annotator.overcluster(adata)
+    mecmodel      = mecs.MetatimeMecs.load_mec_precomputed()
+    mectable      = mecs.load_mecname(mecDIR=config.SCMECDIR, mode='table')
+    mecnamedict   = mecs.getmecnamedict_ct(mectable)
+    pdata         = mecmapper.projectMecAnn(adata, mecmodel.mec_score)
+    projmat, _    = annotator.pdataToTable(pdata, mectable, gcol='overcluster')
+    projmat, _, _ = annotator.annotator(projmat, mecnamedict, gcol='overcluster')
+    projmat       = projmat[['overcluster', 'MetaTiME_overcluster']]
+    projmat       = projmat[['MetaTiME_overcluster']]
+
+    print(adata.obs.shape, projmat.shape)
+    # [issue] Check for duplicate indices in projmat
+    duplicate_indices = projmat.index[projmat.index.duplicated()]
+    if len(duplicate_indices) > 0:
+        print(f"===============> Duplicate indices found in projmat: {len(duplicate_indices)} <===============")
+        projmat = projmat.groupby(projmat.index).first() # Remove duplicate indices except one
+    
+    adata = annotator.saveToAdata(adata, projmat)
+
+    adata.obs['MetaTiME']       = adata.obs['MetaTiME_overcluster'].str.split(': ').str.get(1)
+    adata.obs['Major_MetaTiME'] = adata.obs['MetaTiME'].str.split('_').str.get(0)
+    adata.obs['MetaTiME'].fillna('Unknown', inplace=True)
+    adata.obs['Major_MetaTiME'].fillna('Unknown', inplace=True)
+    # ------------------------------------------------
 
 def plot_embeddings(
         adata: sc.AnnData,
@@ -99,50 +135,66 @@ def run_cell_type_annotation() -> None:
     parser = parse_add_args(parser)
     args   = parser.parse_args()
 
-    disease_id  = args.disease_id
-    dataset_id  = args.dataset_id
-    cellmarker  = args.cellmarker
-    panglaodb   = args.panglaodb
-    foldchange  = args.foldchange
-    pvalue      = args.pvalue
-    celltype    = args.celltype
-    clustertype = args.clustertype
-    rank_rep    = args.rank_rep
+    disease_id   = args.disease_id
+    dataset_id   = args.dataset_id
+    anno_methods = args.anno_methods
+    cellmarker   = args.cellmarker
+    panglaodb    = args.panglaodb
+    foldchange   = args.foldchange
+    pvalue       = args.pvalue
+    celltype     = args.celltype
+    clustertype  = args.clustertype
+    rank_rep     = args.rank_rep
 
     # load existing anndata with case and control
     adata = adata_handler.load_data_case_and_control(disease_id=disease_id, dataset_id=dataset_id)
 
-    # apply SCSA cell type annotation
-    apply_cell_type_anno_scsa(adata=adata, cellmarker=cellmarker, panglaodb=panglaodb,
-                              foldchange=foldchange, pvalue=pvalue, celltype=celltype,
-                              clustertype=clustertype, rank_rep=rank_rep)
-    
-    # plot embeddings
+    # plot clusters and donor embeddings
     plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
-                 plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='leiden.png',
-                 basis='X_mde', color=['leiden'], palette=ov.utils.palette(),
-                 plot_title='Leiden Cluster', xlabel='X_mde1', ylabel='X_mde2',
-                 frameon='small', legend_loc='on data', legend_fontoutline=0.01)
+                    plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='leiden.png',
+                    basis='X_mde', color=['leiden'], palette=ov.utils.palette(),
+                    plot_title='Leiden Cluster', xlabel='X_mde1', ylabel='X_mde2',
+                    frameon='small', legend_loc='on data', legend_fontoutline=0.01)
     plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
-                 plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='scsa_cellmarker.png',
-                 plot_title='SCSA Celltype Annotation (cellmarker)', xlabel='X_mde1', ylabel='X_mde2',
-                 basis='X_mde', color=['scsa_celltype_cellmarker'], palette=ov.utils.palette(),
-                 frameon='small', legend_fontoutline=0.01)
-    plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
-                 plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='scsa_panglaodb.png',
-                 plot_title='SCSA Celltype Annotation (panglaodb)', xlabel='X_mde1', ylabel='X_mde2',
-                 basis='X_mde', color=['scsa_celltype_panglaodb'], palette=ov.utils.palette(),
-                 frameon='small', legend_fontoutline=0.01)
-    plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
-                 plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='donor_cells.png',
-                 plot_title='Donors (Case vs Control)', xlabel='X_mde1', ylabel='X_mde2',
-                 basis='X_mde', color=['donor'], palette=ov.utils.red_palette(),
-                 frameon='small', legend_fontoutline=2)
-    
-    # calculate Ro/e and plot if Ro/e calculated successfully
-    roe = calculate_roe(adata=adata, sample_key='donor', cell_type_key='scsa_celltype_cellmarker')
-    if roe is not None:
-        plot_roe(roe=roe, disease_id=disease_id, dataset_id=dataset_id)
+                    plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='donor_cells.png',
+                    plot_title='Donors (Case vs Control)', xlabel='X_mde1', ylabel='X_mde2',
+                    basis='X_mde', color=['donor'], palette=ov.utils.red_palette(),
+                    frameon='small', legend_fontoutline=2)
+
+    # if SCSA methods given as args, then apply SCSA cell type annotation and plot corresponding embeddings
+    if 'scsa' in anno_methods:
+        apply_cell_type_anno_scsa(adata=adata, cellmarker=cellmarker, panglaodb=panglaodb,
+                                  foldchange=foldchange, pvalue=pvalue, celltype=celltype,
+                                  clustertype=clustertype, rank_rep=rank_rep)
+        plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
+                        plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='scsa_cellmarker.png',
+                        plot_title='SCSA Celltype Annotation (cellmarker)', xlabel='X_mde1', ylabel='X_mde2',
+                        basis='X_mde', color=['scsa_celltype_cellmarker'], palette=ov.utils.palette(),
+                        frameon='small', legend_fontoutline=0.01)
+        plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
+                        plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='scsa_panglaodb.png',
+                        plot_title='SCSA Celltype Annotation (panglaodb)', xlabel='X_mde1', ylabel='X_mde2',
+                        basis='X_mde', color=['scsa_celltype_panglaodb'], palette=ov.utils.palette(),
+                        frameon='small', legend_fontoutline=0.01)
+        
+        # calculate Ro/e and plot if Ro/e calculated successfully
+        roe = calculate_roe(adata=adata, sample_key='donor', cell_type_key='scsa_celltype_cellmarker')
+        if roe is not None:
+            plot_roe(roe=roe, disease_id=disease_id, dataset_id=dataset_id)
+
+    # if MetaTiME methods given as args, then apply celltype annotation with MetaTiME and plot corresponding embeddings
+    if 'metatime' in anno_methods:
+        apply_cell_type_anno_metatime(adata=adata)
+        plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
+                        plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='metatime_major.png',
+                        plot_title='MetaTiME Celltype Annotation (major)',
+                        basis='X_mde', color=['Major_MetaTiME'], palette=ov.utils.palette(),
+                        frameon=False, ncols=1, legend_fontoutline=0.01)
+        plot_embeddings(adata=adata, disease_id=disease_id, dataset_id=dataset_id,
+                        plot_function=ov.utils.embedding, fig_size=(4,4), plot_fname='metatime_minor.png',
+                        plot_title='MetaTiME Celltype Annotation (minor)',
+                        basis='X_mde', color=['MetaTiME'], palette=sns.color_palette("tab20c", 50).as_hex(),
+                        frameon=False, ncols=1, legend_fontoutline=0.01)
 
     # Save adata
     adata_handler.save_adata(adata=adata, disease_id=disease_id, dataset_id=dataset_id)
