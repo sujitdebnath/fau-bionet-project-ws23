730c880c9928fd2ea59d44c3c06a60504979e60d
Sujit Debnath <sujit.debnath.bd@gmail.com>
2024-04-11 11:21:13 +0200

    Updated annotation plots of summary page

--- dashboard/pages/1_Summary.py
+++ dashboard/pages/1_Summary.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 import streamlit as st
 import seaborn as sns
@@ -10,7 +11,7 @@ BASE_DIR      = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(
 BASE_RES_DIR  = os.path.join(BASE_DIR, 'results')
 
 def read_csv(csv_fpath):
-    return pd.read_csv(csv_fpath)
+    return pd.read_csv(csv_fpath, index_col=0)
 
 def map_cell_anno_method_to_col_name(cell_anno_method):
     if cell_anno_method == "SCSA - cellmarker":
@@ -22,7 +23,7 @@ def map_cell_anno_method_to_col_name(cell_anno_method):
     
     return cell_anno_col_name
 
-def cell_anno_box_plot(df, cell_anno_method):
+def cell_anno_box_plot_based_on_anno_methods(df, cell_anno_method):
     cell_anno_col_name = map_cell_anno_method_to_col_name(cell_anno_method)
     
     # Aggregate counts of cell types for each disease and dataset
@@ -47,11 +48,47 @@ def cell_anno_box_plot(df, cell_anno_method):
         color='disease_id',
         category_orders={cell_anno_col_name: list(total_counts[cell_anno_col_name].values),
                          'disease_id': sorted(df['disease_id'].unique())},
-        title=f'Distribution of Cell Types Across Datasets for Each Disease ({cell_anno_method})',
+        title=f'Distribution of Cell Types for Each Disease ({cell_anno_method})',
         labels={'count': 'Count', cell_anno_col_name: 'Cell Type'},
-        color_discrete_sequence=px.colors.qualitative.Set2,
+        color_discrete_sequence=px.colors.qualitative.Set1,
     )
-    fig.update_traces(boxmean='sd')  # Show mean and standard deviation on the box plot
+    fig.update_traces(quartilemethod="inclusive")  # Show mean and standard deviation on the box plot
+
+    return fig
+
+def cell_anno_box_plot_case_control(df, disease_id, cell_anno_method):
+    cell_anno_col_name = map_cell_anno_method_to_col_name(cell_anno_method)
+    
+    # Aggregate counts of cell types for each disease and dataset
+    agg_df = df.groupby(['disease_id', 'dataset_id', 'donor', cell_anno_col_name]).size().reset_index(name='count')
+
+    # Filter rows with given disease
+    agg_df = agg_df[agg_df['disease_id'] == disease_id]
+
+    # Filter out rows with 'Unknown' cell types
+    agg_df = agg_df[agg_df[cell_anno_col_name] != 'Unknown']
+
+    # Calculate total count of each cell type across all datasets and diseases
+    total_counts = agg_df.groupby(cell_anno_col_name)['count'].sum().reset_index()
+    total_counts = total_counts.sort_values(by='count', ascending=False)  # Sort by count
+
+    # Sort agg_df based on total count order
+    agg_df[cell_anno_col_name] = pd.Categorical(agg_df[cell_anno_col_name], categories=total_counts[cell_anno_col_name], ordered=True)
+    agg_df = agg_df.sort_values(by=cell_anno_col_name)
+
+    # Create a Plotly box plot
+    fig = px.box(
+        agg_df,
+        x=cell_anno_col_name,
+        y='count',
+        color='donor',
+        category_orders={cell_anno_col_name: list(total_counts[cell_anno_col_name].values),
+                         'donor': ['control', 'case']},
+        title=f'Distribution of Cell Types for Case vs Control ({disease_id.upper()} Disease and {cell_anno_method})',
+        labels={'count': 'Count', cell_anno_col_name: 'Cell Type'},
+        color_discrete_sequence=px.colors.qualitative.Set1[0:2][::-1],
+    )
+    fig.update_traces(quartilemethod="inclusive")  # Show mean and standard deviation on the box plot
 
     return fig
 
@@ -101,14 +138,40 @@ def cell_anno_heatmap(df, disease_id, anno_method1, anno_method2):
     # Compute the co-occurrence matrix
     co_occurrence_matrix = df_filtered.groupby([cell_anno_col1, cell_anno_col2]).size().unstack(fill_value=0)
 
+    # Define a custom colorscale with more colors
+    custom_colorscale = [
+        [0.0, '#aaffc3'],  # Mint
+        [0.2, '#4363d8'],  # Blue
+        [0.4, '#e6194B'],  # Red
+        [0.6, '#3cb44b'],  # Green
+        [0.8, '#911eb4'],  # Purple
+        [1.0, '#000075']   # Navy
+    ]
+
     # Create a Plotly heatmap
     fig = go.Figure(data=go.Heatmap(
-        z=co_occurrence_matrix.values,
         x=co_occurrence_matrix.columns,
         y=co_occurrence_matrix.index,
-        colorscale='Viridis',
+        z=co_occurrence_matrix.values,
+        colorscale=custom_colorscale,
         hoverongaps=False
     ))
+
+    # Annotate each cell with the corresponding value
+    for i in range(len(co_occurrence_matrix.index)):
+        for j in range(len(co_occurrence_matrix.columns)):
+            cell_value = co_occurrence_matrix.values[i, j]
+            percentile = np.percentile(co_occurrence_matrix.values.flatten(), 95)
+            font_color = 'white' if cell_value > percentile else 'black'
+
+            if cell_value != 0:
+                fig.add_annotation(
+                    x=co_occurrence_matrix.columns[j],
+                    y=co_occurrence_matrix.index[i],
+                    text=str(cell_value),
+                    showarrow=False,
+                    font=dict(color=font_color)
+                )
 
     # Customize layout
     fig.update_layout(
@@ -171,10 +234,10 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-col1, col2, col3, col4 = st.columns(4)
-
 disease_ids = [dir for dir in os.listdir(BASE_RES_DIR) if os.path.isdir(os.path.join(BASE_RES_DIR, dir)) and dir != 'summary']
 dataset_ids = [dataset_id for disease_id in disease_ids for dataset_id in os.listdir(os.path.join(BASE_RES_DIR, disease_id))]
+
+col1, col2, col3, col4 = st.columns(4)
 
 # Metrics in card-like design
 with col1:
@@ -191,26 +254,46 @@ with col4:
 st.markdown("#### 1. Summary of Cell type Annotation")
 
 # ---------- Summary 1 ----------
-st.markdown("##### 1.1. Distribution of Cell Types (unknown labels excluded)")
-cell_anno_methods = ['SCSA - cellmarker', 'SCSA - panglaodb', 'MetaTiME']
-cell_anno_methods_box = st.selectbox('Select Cell-type Annotation Method', cell_anno_methods, key='1')
+st.markdown("##### 1.1. Distribution of Cell Types Based on Annotation Methods (excluded unknown labels)")
+
+cell_anno_methods     = ['SCSA - cellmarker', 'SCSA - panglaodb', 'MetaTiME']
+cell_anno_methods_box = st.selectbox('Select Cell-type Annotation Method', cell_anno_methods, key='anno1')
 
 cell_anno_df = read_csv(csv_fpath=os.path.join(BASE_RES_DIR, 'summary', 'cell_anno_res.csv'))
 # st.pyplot(cell_anno_box_plot2(df=cell_anno_df, cell_anno_method=cell_anno_methods_box))
-st.plotly_chart(cell_anno_box_plot(df=cell_anno_df, cell_anno_method=cell_anno_methods_box), use_container_width=True)
+st.plotly_chart(cell_anno_box_plot_based_on_anno_methods(
+                                                    df=cell_anno_df,
+                                                    cell_anno_method=cell_anno_methods_box),
+                                                use_container_width=True)
 # ---------- Summary 1 ----------
 
 # ---------- Summary 2 ----------
-st.markdown("##### 1.2. Heatmap of Cell Type Co-occurrence")
+st.markdown("##### 1.2. Distribution of Cell Types for Case vs Control (excluded unknown labels)")
+
+col1, col2 = st.columns(2)
+with col1:
+    anno_methods_dropdown = st.selectbox('Select Cell-type Annotation Method', cell_anno_methods, key='anno2')
+with col2:
+    disease_id_dropdown = st.selectbox('Select Disease', sorted(disease_ids), key='dis1')
+
+st.plotly_chart(cell_anno_box_plot_case_control(
+                                            df=cell_anno_df,
+                                            disease_id=disease_id_dropdown,
+                                            cell_anno_method=anno_methods_dropdown),
+                                        use_container_width=True)
+# ---------- Summary 2 ----------
+
+# ---------- Summary 3 ----------
+st.markdown("##### 1.3. Heatmap of Cell Type Co-occurrence")
 
 col1, col2, col3 = st.columns(3)
 with col1:
-    disease_id = st.selectbox('Select Disease', sorted(disease_ids))
+    disease_id = st.selectbox('Select Disease', sorted(disease_ids), key='dis2')
 with col2:
-    cell_anno_methods_box1 = st.selectbox('Select 1st Annotation Method', cell_anno_methods, key='2')
+    cell_anno_methods_box1 = st.selectbox('Select 1st Annotation Method', cell_anno_methods, key='anno3')
 with col3:
     cell_anno_methods2     = [anno_method for anno_method in cell_anno_methods if anno_method != cell_anno_methods_box1]
-    cell_anno_methods_box2 = st.selectbox('Select 2nd Annotation Method', cell_anno_methods2, key='3')
+    cell_anno_methods_box2 = st.selectbox('Select 2nd Annotation Method', cell_anno_methods2, key='anno4')
 
 st.plotly_chart(cell_anno_heatmap(
                     df=cell_anno_df,
@@ -218,7 +301,7 @@ st.plotly_chart(cell_anno_heatmap(
                     anno_method1=cell_anno_methods_box1,
                     anno_method2=cell_anno_methods_box2),
                 use_container_width=True)
-# ---------- Summary 2 ----------
+# ---------- Summary 3 ----------
 # ---------- Summary of Cell Annotation ----------
 
 # ---------- Summary of DGE Analysis ----------
