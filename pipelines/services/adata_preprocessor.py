e21535e97a803b55c134db0be980f66a439fb78a
Sujit Debnath <sujit.debnath.bd@gmail.com>
2024-04-27 17:00:21 +0200

    Added python docstring

--- pipelines/services/adata_preprocessor.py
+++ pipelines/services/adata_preprocessor.py
@@ -1,10 +1,24 @@
+# Python imports
 import argparse
+
+# Third party imports
 import scanpy as sc
 import omicverse as ov
+
+# Self imports
 import adata_handler
 
 
 def parse_add_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    """
+    Add arguments to the argument parser for preprocessing AnnData objects.
+
+    Parameters:
+        parser (argparse.ArgumentParser): Argument parser object.
+
+    Returns:
+        argparse.ArgumentParser: Updated argument parser object.
+    """
     parser.add_argument('--disease_id', type=str, help='Unique identifier for disease, e.g., diabetesII, mpn')
     parser.add_argument('--dataset_id', type=str, help='Unique identifier of the dataset for that specific disease')
     parser.add_argument('--mito_perc', type=float, default=0.05, help='Percentage of mitochondrial genes threshold for quality control')
@@ -19,11 +33,34 @@ def parse_add_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
     return parser
 
 def apply_quantity_control(adata: sc.AnnData, mito_perc: float, n_umis: int, detected_genes: int) -> sc.AnnData:
+    """
+    Apply quality control to the AnnData object.
+
+    Parameters:
+        adata (sc.AnnData): AnnData object.
+        mito_perc (float): Percentage of mitochondrial genes threshold for quality control.
+        n_umis (int): Number of UMIs threshold for quality control.
+        detected_genes (int): Number of detected genes threshold for quality control.
+
+    Returns:
+        sc.AnnData: Updated AnnData object after quality control.
+    """
     adata = ov.pp.qc(adata, tresh = {'mito_perc': mito_perc, 'nUMIs': n_umis, 'detected_genes': detected_genes})
 
     return adata
 
 def preprocess_adata(adata: sc.AnnData, preprocess_mode: str, n_hvgs: int) -> sc.AnnData:
+    """
+    Preprocess the AnnData object.
+
+    Parameters:
+        adata (sc.AnnData): AnnData object.
+        preprocess_mode (str): Preprocessing mode.
+        n_hvgs (int): Number of highly variable genes to select.
+
+    Returns:
+        sc.AnnData: Updated AnnData object after preprocessing.
+    """
     adata     = ov.pp.preprocess(adata, mode=preprocess_mode, n_HVGs=n_hvgs)
     adata.raw = adata
     adata     = adata[:, adata.var.highly_variable_features]
@@ -31,12 +68,33 @@ def preprocess_adata(adata: sc.AnnData, preprocess_mode: str, n_hvgs: int) -> sc
     return adata
 
 def apply_pca(adata: sc.AnnData, n_pcs: int) -> sc.AnnData:
+    """
+    Apply Principal Component Analysis (PCA) to the AnnData object.
+
+    Parameters:
+        adata (sc.AnnData): AnnData object.
+        n_pcs (int): Number of principal components for PCA.
+
+    Returns:
+        sc.AnnData: Updated AnnData object after PCA.
+    """
     ov.pp.scale(adata)
     ov.pp.pca(adata, layer='scaled', n_pcs=n_pcs)
 
     return adata
 
 def construct_neighbourhood_graph(adata: sc.AnnData, n_neighbors: int, n_pcs: int) -> sc.AnnData:
+    """
+    Construct a neighborhood graph for the AnnData object.
+
+    Parameters:
+        adata (sc.AnnData): AnnData object.
+        n_neighbors (int): Number of neighbors for constructing neighborhood graph.
+        n_pcs (int): Number of principal components for constructing neighborhood graph.
+
+    Returns:
+        sc.AnnData: Updated AnnData object with neighborhood graph.
+    """
     sc.pp.neighbors(adata, n_neighbors=n_neighbors, n_pcs=n_pcs, use_rep='scaled|original|X_pca')
 
     # apply dimensionality reduction for visualization (X_mde=X_umap+GPU)
@@ -45,12 +103,34 @@ def construct_neighbourhood_graph(adata: sc.AnnData, n_neighbors: int, n_pcs: in
     return adata
 
 def apply_clustering(adata: sc.AnnData, cluster_mode: str) -> sc.AnnData:
+    """
+    Apply clustering to the AnnData object.
+
+    Parameters:
+        adata (sc.AnnData): AnnData object.
+        cluster_mode (str): Clustering method.
+
+    Returns:
+        sc.AnnData: Updated AnnData object after clustering.
+    """
     if cluster_mode == 'leiden':
         sc.tl.leiden(adata)
     
     return adata
 
 def main() -> None:
+    """
+    Main function for data preprocessing.
+
+    Parses command-line arguments, loads data, applies preprocessing steps,
+    and saves the preprocessed data.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     parser = argparse.ArgumentParser(description='Data Preprocessor Script')
     parser = parse_add_args(parser)
     args   = parser.parse_args()
@@ -84,7 +164,7 @@ def main() -> None:
     # apply clusters
     adata = apply_clustering(adata=adata, cluster_mode=cluster_mode)
 
-    # Save preprocessed data
+    # save preprocessed data
     adata_handler.save_adata(adata=adata, disease_id=disease_id, dataset_id=dataset_id)
 
 
