148c41fe98c051a95ea0b6550200157fdad95cfb
Sujit Debnath <sujit.debnath.bd@gmail.com>
2024-04-27 16:59:59 +0200

    Added python docstring

--- pipelines/services/adata_handler.py
+++ pipelines/services/adata_handler.py
@@ -1,23 +1,47 @@
+# Python imports
 import os
 import sys
 import argparse
+
+# Third party imports
 import anndata as ad
 import scanpy as sc
 import omicverse as ov
 
+# Self imports
 
+
+# Define the necessary directories of the project
 BASE_DIR      = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 BASE_DATA_DIR = os.path.join(BASE_DIR, 'dataset')
 BASE_RES_DIR  = os.path.join(BASE_DIR, 'results')
 
 
 def parse_add_args(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    """
+    Add arguments related to disease and dataset identifiers to the provided argument parser.
+
+    Parameters:
+        parser (argparse.ArgumentParser): The argument parser to which the arguments will be added.
+
+    Returns:
+        argparse.ArgumentParser: The modified argument parser.
+    """
     parser.add_argument('--disease_id', type=str, help='Unique identifier for disease, e.g., diabetesII, mpn')
     parser.add_argument('--dataset_id', type=str, help='Unique identifier of the dataset for that specific disease')
 
     return parser
 
 def create_directory(dir_path: str) -> str:
+    """
+    Create a directory at the specified path if it does not exist already.
+
+    Parameters:
+        dir_path (str): The path of the directory to be created.
+
+    Returns:
+        str: The path of the directory that was created or already existed.
+    """
     try:
         if not os.path.exists(dir_path):
             os.makedirs(dir_path)
@@ -31,6 +55,16 @@ def create_directory(dir_path: str) -> str:
     return dir_path
 
 def prepare_all_dirs(disease_id: str, dataset_id: str) -> None:
+    """
+    Prepare directories for storing results and temporary data.
+
+    Parameters:
+        disease_id (str): Unique identifier for the disease.
+        dataset_id (str): Unique identifier of the dataset for the specific disease.
+
+    Returns:
+        None
+    """
     # create disease and dataset dir in results
     disease_res_dir = create_directory(dir_path=os.path.join(BASE_RES_DIR, disease_id))
     dataset_res_dir = create_directory(dir_path=os.path.join(disease_res_dir, dataset_id))
@@ -39,6 +73,16 @@ def prepare_all_dirs(disease_id: str, dataset_id: str) -> None:
     temp_adata_dir = create_directory(dir_path=os.path.join(BASE_DIR, 'pipelines', 'temp_adata'))
 
 def load_data_case_and_control(disease_id: str, dataset_id: str) -> sc.AnnData:
+    """
+    Load case and control data for a specific disease and dataset.
+
+    Parameters:
+        disease_id (str): Unique identifier for the disease.
+        dataset_id (str): Unique identifier of the dataset for the specific disease.
+
+    Returns:
+        sc.AnnData: Anndata object containing the loaded data.
+    """
     dataset_path = os.path.join(BASE_DIR, 'pipelines', 'temp_adata', f'{disease_id}_{dataset_id}.h5ad')
 
     if os.path.exists(dataset_path):
@@ -76,6 +120,17 @@ def load_data_case_and_control(disease_id: str, dataset_id: str) -> sc.AnnData:
     return adata
 
 def save_adata(adata: sc.AnnData, disease_id: str, dataset_id: str) -> None:
+    """
+    Save an AnnData object to a file.
+
+    Parameters:
+        adata (sc.AnnData): Anndata object to be saved.
+        disease_id (str): Unique identifier for the disease.
+        dataset_id (str): Unique identifier of the dataset for the specific disease.
+
+    Returns:
+        None
+    """
     try:
         temp_adata_dir = os.path.join(BASE_DIR, 'pipelines', 'temp_adata')
         res_fpath      = os.path.join(temp_adata_dir, f'{disease_id}_{dataset_id}.h5ad')
@@ -87,6 +142,18 @@ def save_adata(adata: sc.AnnData, disease_id: str, dataset_id: str) -> None:
         sys.exit(1)
 
 def main() -> None:
+    """
+    Main function to handle data processing tasks.
+
+    This function parses command-line arguments, prepares directories, loads data,
+    saves data, and performs other necessary tasks.
+
+    Parameters:
+        None
+
+    Returns:
+        None
+    """
     parser = argparse.ArgumentParser(description='Data Handler Script')
     parser = parse_add_args(parser)
     args   = parser.parse_args()
